8c8fac1d7243cefc36ff4e737b81b90ef3df06d8
Ferenc Szalma <szalmaf@users.noreply.github.com>
2016-10-20 13:41:17 -0400

    Initial Commit

---
+++ C++_Hackerrank_AccessingInheritedFunctions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++_Hackerrank_AccessingInheritedFunctions
+//
+//  Created by Ferenc Szalma on 10/20/16.
+//  Copyright © 2016 Ferenc Szalma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
